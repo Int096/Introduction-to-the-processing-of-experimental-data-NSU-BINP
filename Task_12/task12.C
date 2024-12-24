fd58518777dcf5b26afa0ac3dbbfd0f599f09fe7
Egor Kurgin <kurginegor1901@gmail.com>
2024-12-24 22:06:05 +0700

    Task 12 add

--- Task_12/task12.C
+++ Task_12/task12.C
@@ -1,4 +1,5 @@
 #include <TH1D.h>
+#include <TROOT.h>
 #include <TF1.h>
 #include <TCanvas.h>
 #include <TStyle.h>
@@ -9,7 +10,7 @@
 enum ParIndex_t 
 {
     PScale = 0,
-    BWScale,
+    Pmean,
     BWMean,
     BWWidth,
     N_PAR 
@@ -18,7 +19,7 @@ enum ParIndex_t
 const std::map<ParIndex_t, TString> parNames 
 {
     {PScale,  "Poisson scale"},
-    {BWScale, "Breit Wigner scale"},
+    {Pmean,   "Poisson mean"},
     {BWMean,  "Breit Wigner mean"},
     {BWWidth, "Breit Wigner width"},
 };
@@ -37,15 +38,30 @@ void FillHistogram(TH1D *histogram, TString filename)
     }
 }
 
+Double_t bww(Double_t *x, Double_t *par)
+{
+    return TMath::BreitWigner(x[0], par[0], par[1]);
+}
+
+TF1 *bw = new TF1("bw", bww, -10, 10, 2);
+
 Double_t fitFunc(Double_t *x, Double_t *par)
 {
-    return par[PScale]  * TMath::Poisson(x[0], 0.2) 
-         + par[BWScale] * TMath::BreitWigner(x[0], par[BWMean], par[BWWidth]);
+    TH1D *hist;
+    gROOT->GetObject("hist", hist);
+
+    bw->SetParameter(0, par[BWMean]);
+    bw->SetParameter(1, par[BWWidth]);
+    
+    return 
+    par[PScale] * TMath::Poisson(x[0], par[Pmean]) +
+    (150 - par[PScale]) * TMath::BreitWigner(x[0], par[BWMean], par[BWWidth]);
 }
 
 void task12()
 {   
     gStyle->SetOptFit(11111);
+    
     TH1D *histogram = new TH1D("hist", 
                                "Distribution data from task10Nov.dat;mm;yields / 0.1mm",
                                100, 0, 10);
@@ -53,9 +69,19 @@ void task12()
     FillHistogram(histogram, "task10Nov.dat");
 
     TF1 *fit = new TF1("fit", fitFunc, 0, 10, 4);
-    for (auto i : {PScale, BWScale, BWMean, BWWidth}) {
-        fit->SetParameter(i, 1);
-        fit->SetParLimits(i, 0, 1000);
+
+    // Set parameters
+    fit->SetParameter(PScale, 1000);
+    fit->SetParameter(Pmean, 0.2);
+    fit->SetParameter(BWMean, 5);
+    fit->SetParameter(BWWidth, 10);
+
+    fit->SetParLimits(PScale, 0, histogram->GetSum());
+    fit->SetParLimits(Pmean, 0, 100);
+    fit->SetParLimits(BWMean, 1, 10);
+    fit->SetParLimits(BWWidth, 0, 100);
+
+    for (auto i : {PScale, Pmean, BWMean, BWWidth}) {
         fit->SetParName(i, (*parNames.find(i)).second);
     }
 
